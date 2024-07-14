e265fda2e1132e4c619d0f7f51d60aa6b731691c
iq <888.quliyev.888@gmail.com>
2024-07-14 07:41:36 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,18 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
+/**
+ * struct dog - This structure defines a dog.
+ *
+ * @name: Pointer to a character string holding the dog's name.
+ * @age: Floating point number representing the dog's age.
+ * @owner: Pointer to a character string holding the owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
