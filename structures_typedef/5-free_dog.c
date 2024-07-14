ef371e840cb48250ea7405cf7d2ce5eb23af1843
iq <888.quliyev.888@gmail.com>
2024-07-14 11:14:01 +0000

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_dog - Frees memory allocated for a dog structure.
+ *
+ * @d: Pointer to allocated memory for a dog structure.
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
