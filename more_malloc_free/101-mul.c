2cd4248857fbabb98e549f356c343baa4d6b53e3
iq <888.quliyev.888@gmail.com>
2024-07-14 17:26:47 +0000

    .

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -45,7 +45,10 @@ char *big_multiply(char *s1, char *s2)
 	l2 = _strlen(s2);
 	r = malloc(a = x = l1 + l2);
 	if (!r)
+	{
+		free(r);
 		printf("Error\n"), exit(98);
+	}
 	while (a--)
 		r[a] = 0;
 
