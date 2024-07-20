20286c3c82c08c078472dca1503565266ff0db23
iq <888.quliyev.888@gmail.com>
2024-07-20 14:26:58 +0000

    .

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,19 +12,21 @@ printf("Error\n");
 return (98);
 }
 
-if (argv[2][1] != 0)
-{	
+if (argv[2][0] == '/' && argv[3][0] == '0')
+{
 printf("Error\n");
-return (99);
+return (100);
 }
 
 fptr = get_op_func(argv[2]);
 
-if (fptr == NULL)
+
+if (argv[2][1] != 0 || fptr == NULL)
 {
-	printf("Error\n");
-	return (99);
+printf("Error\n");
+return (99);
 }
+
 printf("%d\n", (*fptr)(atoi(argv[1]), atoi(argv[3])));
 
 return (0);
