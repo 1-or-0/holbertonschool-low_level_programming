40595995b80f11d1953152f007d0cbebdf126541
iq <888.quliyev.888@gmail.com>
2024-07-20 10:52:58 +0000

    function_pointers.h

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -6,9 +6,9 @@
  * @size: Size of the array.
  * @action: Pointer to the given function.
  */
-void array_iterator(int *array, unsigned int size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	size_t i = 0;
 
 	while (i < size)
 	{
