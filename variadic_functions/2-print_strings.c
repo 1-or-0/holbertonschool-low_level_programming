b9860ef9bee1d4d1aa85297b354729d188ce5061
iq <888.quliyev.888@gmail.com>
2024-07-20 22:11:43 +0000

    2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -22,12 +22,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < (int)n; i++)
 	{
-
-
-		if (va_arg(ap, char*) == NULL)
-			printf("nil");
-		
-		else
 			printf("%s", va_arg(ap, char*));
 
 		if (i + 1 >= (int)n)
