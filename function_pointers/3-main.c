74139a2e8af8f12a4c0c569af50e45a16689aa30
iq <888.quliyev.888@gmail.com>
2024-07-20 13:14:28 +0000

    .

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,14 +8,14 @@ int (*fptr)(int, int);
 
 if (argc != 4)
 {
-printf("ERROR\n");
+printf("Error\n");
 return (98);
 }
 
 if (argv[2][1] != 0)
 {	
 printf("Error\n");
-return (0);
+return (98);
 }
 
 fptr = get_op_func(argv[2]);
