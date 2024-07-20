08e705946e9ec3585249b8f25f537fb2be540099
iq <888.quliyev.888@gmail.com>
2024-07-20 12:46:38 +0000

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+
+int (*fptr)(int, int);
+
+if (argc != 4)
+{
+printf("ERROR\n");
+return (0);
+}
+
+if (argv[2][1] != 0)
+{	
+printf("Error\n");
+return (0);
+}
+
+fptr = get_op_func(argv[2]);
+
+printf("%d\n", (*fptr)(atoi(argv[1]), atoi(argv[3])));
+
+return (0);
+}
